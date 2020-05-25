387432744eeb78eea5c5fa1a5cbdad517c71b46c
SYZ1234 <65107905+SYZ1234@users.noreply.github.com>
2020-05-25 17:05:56 +0800

    Add files via upload

--- motion_code_original.R
+++ motion_code_original.R
@@ -1,23 +1,4 @@
----
-title: "RepData_assignment_SYZ"
-author: "SYZ"
-date: "5/25/2020"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-
-
-## R Markdown assignment (Coursera)
-
-This is an assignment in Coursera to analyze personal movement using activity monitoring devices.
-
-1. Loading and preprocessing the data
-```{r}
-#Author:SYZ
+Author:SYZ
 
 library(data.table)
 
@@ -31,42 +12,26 @@ data <- read.csv('./activity.csv')
 str(data)
 #date
 data$date <- as.Date(data$date, format = "%Y-%m-%d")
-```
 
-
-2. What is mean total number of steps taken per day?
-```{r}
 ###2. What is mean total number of steps taken per day?
 #Histogram of total number of steps per day
 steps_sum <- tapply(data$steps,data$date,sum,na.rm=T)
 barplot(steps_sum,xlab='date',ylab='total steps',main='total steps per day')
-```
 
-3. Mean and median number of steps taken each day
-```{r}
 ###3. Mean and median number of steps taken each day
 steps_mean <- mean(steps_sum,na.rm=T)
 print(steps_mean) #9354.23
 #Calculate median
 steps_median <- median(steps_sum,na.rm=T)
 print(steps_median) #10395
-```
 
-4. Time series plot of the average number of steps taken
-```{r}
 ###4. Time series plot of the average number of steps taken
 steps_average <- tapply(data$steps,data$interval,mean,na.rm=T)
 plot(steps_average,xlab='5-minute interval',ylab='average steps',main='time series',type='l')
-```
 
-5. The 5-minute interval that, on average, contains the maximum number of steps
-```{r}
 ###5. The 5-minute interval that, on average, contains the maximum number of steps
 which.max(steps_average) #835, 104
-```
 
-6. Code to describe and show a strategy for imputing missing data
-```{r}
 ###6. Code to describe and show a strategy for imputing missing data
 #describe dataset (data$steps) with NA
 str(data)
@@ -75,17 +40,12 @@ sum(is.na(data$steps)) #2304 missing
 library(mice)
 data_impute <- mice(data) #default m=5 number of imputed datasets, method: pmm
 data_complete <-complete(data_impute,5) #May select any 1 out of the 5 imputed datasets
-```
 
-7. Histogram of the total number of steps taken each day after missing values are imputed
-```{r}
+
 ###7. Histogram of the total number of steps taken each day after missing values are imputed
 steps_sum_complete <- tapply(data_complete$steps, data_complete$date, sum, na.rm=T)
 barplot(steps_sum_complete, xlab='date', ylab='total steps',main='total steps (impute)')
-```
 
-8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
-```{r}
 ###8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
 library(ggplot2)
 data_complete$date <- as.Date(data_complete$date, format = "%Y-%m-%d")
@@ -96,10 +56,3 @@ g<-ggplot(data_complete,aes(interval,steps,color=Week))
 g+facet_grid(rows=vars(Week))+geom_line(stat='summary', fun.y='mean')
 
 ###9. All of the R code needed to reproduce the results (numbers, plots, etc.) in the report
-```
-
-
-
-```{r}
-#Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
-```
